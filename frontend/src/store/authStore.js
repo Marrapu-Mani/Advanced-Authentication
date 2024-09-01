53bc13aa25fe52709633adc8240babc9cb61a670
Git Practice <marrapumani10@gmail.com>
2024-09-02 00:11:57 +0530

    URL updated in authStore

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -1,7 +1,7 @@
 import { create } from 'zustand';
 import axios from 'axios';
 
-const API_URL = "http://localhost:5000/auth";
+const API_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/auth" : "/auth";
 
 axios.defaults.withCredentials = true;
 
